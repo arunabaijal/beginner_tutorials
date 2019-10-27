34231ffc369a7bae936b36172da4d9bcc67cddd7
Aruna Baijal <aruna11baijal@gmail.com>
2019-10-27 14:03:53 -0400

    [Talker.cpp]Editing talker to send custom message

--- src/talker.cpp
+++ src/talker.cpp
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "Greetings from Aruna! " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
