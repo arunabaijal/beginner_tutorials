1fafa9fbace6299fdfc80a9c01b473bf2b4e51ee
Aruna Baijal <aruna11baijal@gmail.com>
2019-11-11 11:39:41 -0500

    [talkerTest.cpp] Adding comments

--- test/talkerTest.cpp
+++ test/talkerTest.cpp
@@ -1,21 +1,63 @@
+/*@
+ *@file talkerTest.cpp
+ *@author Aruna Baijal
+ *@brief This is the test for publisher class.
+ *@copyright 2019 Aruna Baijal
+ */
+/**
+ *Copyright 2019, Aruna
+ *
+ *Redistribution and use in source and binary forms, with or without
+ *modification, are permitted provided that the following conditions are met:
+ *
+ *1. Redistributions of source code must retain the above copyright notice, this
+ *list of conditions and the following disclaimer.
+ *
+ *2. Redistributions in binary form must reproduce the above copyright notice,
+ *this list of conditions and the following disclaimer in the documentation
+ *and/or other materials provided with the distribution.
+ *
+ *3. Neither the name of the copyright holder nor the names of its contributors
+ *may be used to endorse or promote products derived from this software without
+ *specific prior written permission.
+ *
+ *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *OF THISSOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
 #include <ros/ros.h>
 #include <ros/service_client.h>
 #include <gtest/gtest.h>
 #include "beginner_tutorials/ModifyMessage.h"
 #include "std_msgs/String.h"
 
-TEST(TESTSuite, ModifyMessage)
+
+/**
+ * @brief Test case to check whether the ModifyMessage service
+ *        exists and changes publishing string.
+ * @param none
+ * @return none
+ */
+TEST(TestSuite, ServiceTest)
 {
   ros::NodeHandle n;
   ros::ServiceClient client = n.serviceClient<beginner_tutorials::ModifyMessage>(
       "ModifyMessage");
-  bool exists(client.waitForExistence(ros::Duration(5)));
+  bool exists(client.waitForExistence(ros::Duration(5))); // checking for service existence
   EXPECT_TRUE(exists);
 
   beginner_tutorials::ModifyMessage srv;
   srv.request.changeString = "Test String";
   client.call(srv.request, srv.response);
-  EXPECT_EQ("Test String", srv.response.changedString);
+  EXPECT_EQ("Test String", srv.response.changedString); // checking whether publishing string changed
 }
 
 // Run all the tests that were declared with TEST()
