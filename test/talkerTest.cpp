a75643919d3d520915e72adfb8e940780471506c
Aruna Baijal <aruna11baijal@gmail.com>
2019-11-10 23:13:17 -0500

    [talkerTest.cpp]Correcting typo

--- test/talkerTest.cpp
+++ test/talkerTest.cpp
@@ -4,19 +4,18 @@
 #include "beginner_tutorials/ModifyMessage.h"
 #include "std_msgs/String.h"
 
-TEST(TESTSuite, modifyMessage)
+TEST(TESTSuite, ModifyMessage)
 {
   ros::NodeHandle n;
   ros::ServiceClient client = n.serviceClient<beginner_tutorials::ModifyMessage>(
-      "modifyMessage");
-  bool exists(client.waitForExistence(ros::Duration(1)));
+      "ModifyMessage");
+  bool exists(client.waitForExistence(ros::Duration(5)));
   EXPECT_TRUE(exists);
 
   beginner_tutorials::ModifyMessage srv;
   srv.request.changeString = "Test String";
-  client.call(srv);
-  std::string expected = srv.response.changedString;
-  EXPECT_EQ(expected, srv.request.changeString);
+  client.call(srv.request, srv.response);
+  EXPECT_EQ("Test String", srv.response.changedString);
 }
 
 // Run all the tests that were declared with TEST()
